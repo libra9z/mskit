9dac71b182477f26ba95ea464e3bd1b713749717
eric <libra9z@163.com>
2021-07-17 22:41:45 +0800

    增加context内容

--- go.mod
+++ go.mod
@@ -1,20 +1,16 @@
 module github.com/libra9z/mskit
 
-go 1.14
+go 1.16
 
 require (
-	github.com/disintegration/imaging v1.6.2 // indirect
-	github.com/fgrid/uuid v0.1.0 // indirect
-	github.com/go-kit/kit v0.10.0
-	github.com/hashicorp/consul/api v1.6.0
+	github.com/go-kit/kit v0.11.0
+	github.com/hashicorp/consul/api v1.9.1
 	github.com/libra9z/httprouter v1.2.0
-	github.com/libra9z/mahonia v0.0.0-20150731031913-6d9f3cf9f957 // indirect
-	github.com/libra9z/utils v0.0.0-20200531104440-670e4d45d67d
-	github.com/nacos-group/nacos-sdk-go v1.0.1
+	github.com/libra9z/utils v1.0.3
+	github.com/nacos-group/nacos-sdk-go v1.0.8
 	github.com/opentracing/opentracing-go v1.2.0
-	github.com/openzipkin/zipkin-go v0.2.3
-	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0
-	github.com/smallnest/rpcx v0.0.0-20200822084631-5599102026f5
+	github.com/openzipkin/zipkin-go v0.2.5
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475
+	github.com/smallnest/rpcx v1.6.4
+	google.golang.org/grpc/examples v0.0.0-20210715165331-ce7bdf50abb1 // indirect
 )
-
-replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
--- rest/request.go
+++ rest/request.go
@@ -1,10 +1,14 @@
 package rest
 
 import (
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+	"time"
+
 	"github.com/libra9z/httprouter"
 	"github.com/libra9z/mskit/trace"
-	"net/http"
-	"strconv"
 )
 
 type Request struct {
@@ -18,9 +22,27 @@ type Request struct {
 	RemoteAddr    string
 	OriginRequest *http.Request
 	ContentType   int
-	Userid		  string  //admin user prefix with 'a' ,user table prefix with 'u'
+	Userid        string //admin user prefix with 'a' ,user table prefix with 'u'
 	Tracer        trace.Tracer
 	AuthedOrgids  []int64
+
+	// This mutex protect Keys map
+	mu sync.RWMutex
+
+	// queryCache use url.ParseQuery cached the param query result from c.Request.URL.Query()
+	queryCache url.Values
+
+	// formCache use url.ParseQuery cached PostForm contains the parsed form data from POST, PATCH,
+	// or PUT body parameters.
+	formCache url.Values
+
+	// SameSite allows a server to define a cookie attribute making it impossible for
+	// the browser to send this cookie along with cross-site requests.
+	sameSite http.SameSite
+}
+
+type Context struct {
+	Request
 }
 
 const (
@@ -29,7 +51,7 @@ const (
 	CONTENT_TYPE_XML       = 2
 	CONTENT_TYPE_JSON      = 3
 	CONTENT_TYPE_MULTIFORM = 4
-	CONTENT_TYPE_SENML 		= 5
+	CONTENT_TYPE_SENML     = 5
 )
 
 func NewRequest() *Request {
@@ -38,52 +60,52 @@ func NewRequest() *Request {
 	}
 }
 
-func (r *Request) GetString(key string) []string {
-	var ret []string
-	for k, v := range r.Queries {
-		if k == key {
-			ret = v.([]string)
-			break
-		}
-	}
+// func (r *Request) GetString(key string) []string {
+// 	var ret []string
+// 	for k, v := range r.Queries {
+// 		if k == key {
+// 			ret = v.([]string)
+// 			break
+// 		}
+// 	}
 
-	return ret
-}
-func (r *Request) GetInt(key string) []int {
-	var ret []int
+// 	return ret
+// }
+// func (r *Request) GetInt(key string) []int {
+// 	var ret []int
 
-	for k, v := range r.Queries {
-		if k == key {
-			s := v.([]string)
+// 	for k, v := range r.Queries {
+// 		if k == key {
+// 			s := v.([]string)
 
-			for _, si := range s {
-				iv, _ := strconv.ParseInt(si, 10, 64)
-				ret = append(ret, int(iv))
-			}
+// 			for _, si := range s {
+// 				iv, _ := strconv.ParseInt(si, 10, 64)
+// 				ret = append(ret, int(iv))
+// 			}
 
-			break
-		}
-	}
-	return ret
-}
+// 			break
+// 		}
+// 	}
+// 	return ret
+// }
 
-func (r *Request) GetInt64(key string) []int64 {
-	var ret []int64
+// func (r *Request) GetInt64(key string) []int64 {
+// 	var ret []int64
 
-	for k, v := range r.Queries {
-		if k == key {
-			s := v.([]string)
+// 	for k, v := range r.Queries {
+// 		if k == key {
+// 			s := v.([]string)
 
-			for _, si := range s {
-				iv, _ := strconv.ParseInt(si, 10, 64)
-				ret = append(ret, iv)
-			}
+// 			for _, si := range s {
+// 				iv, _ := strconv.ParseInt(si, 10, 64)
+// 				ret = append(ret, iv)
+// 			}
 
-			break
-		}
-	}
-	return ret
-}
+// 			break
+// 		}
+// 	}
+// 	return ret
+// }
 
 func (r *Request) SetAuthorized(auth bool) {
 	r.IsAuthorized = auth
@@ -112,4 +134,248 @@ func (r *Request) GetContentType() string {
 		ct = "multipart/form-data"
 	}
 	return ct
+}
+
+/************************************/
+/******** METADATA MANAGEMENT********/
+/************************************/
+
+// Set is used to store a new key/value pair exclusively for this Request.
+// It also lazy initializes  c.Keys if it was not used previously.
+func (c *Request) Set(key string, value interface{}) {
+	c.mu.Lock()
+	if c.Queries == nil {
+		c.Queries = make(map[string]interface{})
+	}
+
+	c.Queries[key] = value
+	c.mu.Unlock()
+}
+
+// Get returns the value for the given key, ie: (value, true).
+// If the value does not exists it returns (nil, false)
+func (c *Request) Get(key string) (value interface{}, exists bool) {
+	c.mu.RLock()
+	value, exists = c.Queries[key]
+	c.mu.RUnlock()
+	return
+}
+
+// MustGet returns the value for the given key if it exists, otherwise it panics.
+func (c *Request) MustGet(key string) interface{} {
+	if value, exists := c.Get(key); exists {
+		return value
+	}
+	panic("Key \"" + key + "\" does not exist")
+}
+
+// GetString returns the value associated with the key as a string.
+func (c *Request) GetString(key string) (s string) {
+	if val, ok := c.Get(key); ok && val != nil {
+		s, _ = val.(string)
+	}
+	return
+}
+
+// GetBool returns the value associated with the key as a boolean.
+func (c *Request) GetBool(key string) (b bool) {
+	if val, ok := c.Get(key); ok && val != nil {
+		b, _ = val.(bool)
+	}
+	return
+}
+
+// GetInt returns the value associated with the key as an integer.
+func (c *Request) GetInt(key string) (i int) {
+	if val, ok := c.Get(key); ok && val != nil {
+		i, _ = val.(int)
+	}
+	return
+}
+
+// GetInt64 returns the value associated with the key as an integer.
+func (c *Request) GetInt64(key string) (i64 int64) {
+	if val, ok := c.Get(key); ok && val != nil {
+		i64, _ = val.(int64)
+	}
+	return
+}
+
+// GetFloat64 returns the value associated with the key as a float64.
+func (c *Request) GetFloat64(key string) (f64 float64) {
+	if val, ok := c.Get(key); ok && val != nil {
+		f64, _ = val.(float64)
+	}
+	return
+}
+
+// GetTime returns the value associated with the key as time.
+func (c *Request) GetTime(key string) (t time.Time) {
+	if val, ok := c.Get(key); ok && val != nil {
+		t, _ = val.(time.Time)
+	}
+	return
+}
+
+// GetDuration returns the value associated with the key as a duration.
+func (c *Request) GetDuration(key string) (d time.Duration) {
+	if val, ok := c.Get(key); ok && val != nil {
+		d, _ = val.(time.Duration)
+	}
+	return
+}
+
+// GetInt returns the value associated with the key as an integer.
+func (c *Request) GetIntSlice(key string) (i []int) {
+	if val, ok := c.Get(key); ok && val != nil {
+		i, _ = val.([]int)
+	}
+	return
+}
+
+// GetInt64 returns the value associated with the key as an integer.
+func (c *Request) GetInt64Slice(key string) (i64 []int64) {
+	if val, ok := c.Get(key); ok && val != nil {
+		i64, _ = val.([]int64)
+	}
+	return
+}
+
+// GetStringSlice returns the value associated with the key as a slice of strings.
+func (c *Request) GetStringSlice(key string) (ss []string) {
+	if val, ok := c.Get(key); ok && val != nil {
+		ss, _ = val.([]string)
+	}
+	return
+}
+
+// GetStringMap returns the value associated with the key as a map of interfaces.
+func (c *Request) GetStringMap(key string) (sm map[string]interface{}) {
+	if val, ok := c.Get(key); ok && val != nil {
+		sm, _ = val.(map[string]interface{})
+	}
+	return
+}
+
+// GetStringMapString returns the value associated with the key as a map of strings.
+func (c *Request) GetStringMapString(key string) (sms map[string]string) {
+	if val, ok := c.Get(key); ok && val != nil {
+		sms, _ = val.(map[string]string)
+	}
+	return
+}
+
+// GetStringMapStringSlice returns the value associated with the key as a map to a slice of strings.
+func (c *Request) GetStringMapStringSlice(key string) (smss map[string][]string) {
+	if val, ok := c.Get(key); ok && val != nil {
+		smss, _ = val.(map[string][]string)
+	}
+	return
+}
+
+/************************************/
+/************ INPUT DATA ************/
+/************************************/
+
+// Param returns the value of the URL param.
+// It is a shortcut for c.Params.ByName(key)
+//     router.GET("/user/:id", func(c *gin.Request) {
+//         // a GET request to /user/john
+//         id := c.Param("id") // id == "john"
+//     })
+func (c *Request) Param(key string) string {
+	return c.Params.ByName(key)
+}
+
+// Query returns the keyed url query value if it exists,
+// otherwise it returns an empty string `("")`.
+// It is shortcut for `c.Request.URL.Query().Get(key)`
+//     GET /path?id=1234&name=Manu&value=
+// 	   c.Query("id") == "1234"
+// 	   c.Query("name") == "Manu"
+// 	   c.Query("value") == ""
+// 	   c.Query("wtf") == ""
+func (c *Request) Query(key string) string {
+	value, _ := c.GetQuery(key)
+	return value
+}
+
+// DefaultQuery returns the keyed url query value if it exists,
+// otherwise it returns the specified defaultValue string.
+// See: Query() and GetQuery() for further information.
+//     GET /?name=Manu&lastname=
+//     c.DefaultQuery("name", "unknown") == "Manu"
+//     c.DefaultQuery("id", "none") == "none"
+//     c.DefaultQuery("lastname", "none") == ""
+func (c *Request) DefaultQuery(key, defaultValue string) string {
+	if value, ok := c.GetQuery(key); ok {
+		return value
+	}
+	return defaultValue
+}
+
+// GetQuery is like Query(), it returns the keyed url query value
+// if it exists `(value, true)` (even when the value is an empty string),
+// otherwise it returns `("", false)`.
+// It is shortcut for `c.Request.URL.Query().Get(key)`
+//     GET /?name=Manu&lastname=
+//     ("Manu", true) == c.GetQuery("name")
+//     ("", false) == c.GetQuery("id")
+//     ("", true) == c.GetQuery("lastname")
+func (c *Request) GetQuery(key string) (string, bool) {
+	if values, ok := c.GetQueryArray(key); ok {
+		return values[0], ok
+	}
+	return "", false
+}
+
+// QueryArray returns a slice of strings for a given query key.
+// The length of the slice depends on the number of params with the given key.
+func (c *Request) QueryArray(key string) []string {
+	values, _ := c.GetQueryArray(key)
+	return values
+}
+
+func (c *Request) getQueryCache() {
+	if c.queryCache == nil {
+		c.queryCache = c.OriginRequest.URL.Query()
+	}
+}
+
+// GetQueryArray returns a slice of strings for a given query key, plus
+// a boolean value whether at least one value exists for the given key.
+func (c *Request) GetQueryArray(key string) ([]string, bool) {
+	c.getQueryCache()
+	if values, ok := c.queryCache[key]; ok && len(values) > 0 {
+		return values, true
+	}
+	return []string{}, false
+}
+
+// QueryMap returns a map for a given query key.
+func (c *Request) QueryMap(key string) map[string]string {
+	dicts, _ := c.GetQueryMap(key)
+	return dicts
+}
+
+// GetQueryMap returns a map for a given query key, plus a boolean value
+// whether at least one value exists for the given key.
+func (c *Request) GetQueryMap(key string) (map[string]string, bool) {
+	c.getQueryCache()
+	return c.get(c.queryCache, key)
+}
+
+// get is an internal method and returns a map which satisfy conditions.
+func (c *Request) get(m map[string][]string, key string) (map[string]string, bool) {
+	dicts := make(map[string]string)
+	exist := false
+	for k, v := range m {
+		if i := strings.IndexByte(k, '['); i >= 1 && k[0:i] == key {
+			if j := strings.IndexByte(k[i+1:], ']'); j >= 1 {
+				exist = true
+				dicts[k[i+1:][:j]] = v[0]
+			}
+		}
+	}
+	return dicts, exist
 }
